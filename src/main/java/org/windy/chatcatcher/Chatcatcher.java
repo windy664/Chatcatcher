e619cd50f854f17cbbac0d281d56e032525d6c0b
风吟 <2580855214@qq.com>
2024-02-04 10:50:27 +0800

    我也不知道改了啥

--- src/main/java/org/windy/chatcatcher/Chatcatcher.java
+++ src/main/java/org/windy/chatcatcher/Chatcatcher.java
@@ -24,7 +24,6 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             Bukkit.getPluginManager().registerEvents(this, this);
         } else {
@@ -32,17 +31,13 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
             Bukkit.getPluginManager().disablePlugin(this);
         }
 
-        // 保存默认配置文件
         saveDefaultConfig();
 
-        // 加载配置文件
         configFile = new File(getDataFolder(), "config.yml");
         config = YamlConfiguration.loadConfiguration(configFile);
 
-        // 初始化捕获器数据Map
         catcherDataMap = new HashMap<>();
 
-        // 注册命令
         Objects.requireNonNull(getCommand("chatcatcher")).setExecutor(this);
         Objects.requireNonNull(getCommand("setcatcher")).setExecutor(this);
         this.getServer().getConsoleSender().sendMessage(Texts.logo);
@@ -50,7 +45,6 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        // 在插件卸载时保存配置文件
         saveConfig();
         this.getServer().getConsoleSender().sendMessage(Texts.logo);
     }
@@ -62,15 +56,12 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
                 String catcherName = args[0];
                 String playerName = args[1];
 
-                // 获取捕获器配置
                 ConfigurationSection catcherConfig = config.getConfigurationSection("catchers." + catcherName);
                 if (catcherConfig != null) {
                     String catcherType = catcherConfig.getString("type", "CHAT");
 
-                    // 初始化捕获器数据
                     catcherDataMap.put(Bukkit.getPlayerExact(playerName).getUniqueId(), new CatcherData(catcherName, catcherType));
 
-                    // 发送开始信息
                     catcherConfig.getStringList("start").forEach(sender::sendMessage);
                 } else {
                     sender.sendMessage("找不到指定的捕获器：" + catcherName);
@@ -85,16 +76,12 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
             if (args.length == 1) {
                 String catcherName = args[0];
 
-                // 检查配置中是否有该捕获器
                 ConfigurationSection catcherConfig = config.getConfigurationSection("catchers." + catcherName);
                 if (catcherConfig != null) {
-                    // 获取捕获器配置
                     String catcherType = catcherConfig.getString("type", "CHAT");
 
-                    // 初始化捕获器数据
                     catcherDataMap.put(player.getUniqueId(), new CatcherData(catcherName, catcherType));
 
-                    // 发送开始信息
                     catcherConfig.getStringList("start").forEach(player::sendMessage);
                 } else {
                     player.sendMessage("找不到指定的捕获器：" + catcherName);
@@ -117,34 +104,25 @@ public final class Chatcatcher extends JavaPlugin implements Listener {
             String catcherName = catcherData.getCatcherName();
             String catcherType = catcherData.getCatcherType();
 
-            // 获取捕获器配置
             ConfigurationSection catcherConfig = config.getConfigurationSection("catchers." + catcherName);
             if (catcherConfig != null) {
-                // 获取捕获器类型配置
                 String type = catcherConfig.getString("type", "CHAT");
 
                 if (type.equalsIgnoreCase(catcherType)) {
-                    // 执行捕获器配置的操作
                     catcherConfig.getStringList("cancel").forEach(player::sendMessage);
 
-                    // 动态生成变量
                     String inputVariable = "{input:" + event.getMessage() + "}";
 
-                    // 替换配置中的变量
                     List<String> endMessages = catcherConfig.getStringList("end").stream()
                             .map(message -> PlaceholderAPI.setPlaceholders(player, message.replace("{meta:input}", inputVariable)))
                             .collect(Collectors.toList());
 
-                    // 发送结束信息
                     endMessages.forEach(player::sendMessage);
 
-                    // 执行命令
                     endMessages.forEach(command -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command));
 
-                    // 移除捕获器数据
                     catcherDataMap.remove(playerUUID);
                 } else {
-                    // 发送捕获器类型不匹配的消息
                     player.sendMessage("捕获器类型不匹配。");
                 }
             }
