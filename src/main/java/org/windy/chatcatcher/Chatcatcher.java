d9cd2ed7f80a392a1cfbc365870b8b0e2f685fbd
风吟 <2580855214@qq.com>
2024-02-04 10:31:57 +0800

    Initial commit

---
+++ src/main/java/org/windy/chatcatcher/Chatcatcher.java
@@ -0,0 +1,17 @@
+package org.windy.chatcatcher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chatcatcher extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
